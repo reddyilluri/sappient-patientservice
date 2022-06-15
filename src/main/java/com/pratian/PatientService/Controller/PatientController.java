9cd1055c4c4de927808b7944d69b673eecf4f63e
Archish Pandey <apsibbu@gmail.com>
2022-06-15 20:47:11 +0530

    Updated patient controller

--- src/main/java/com/pratian/PatientService/Controller/PatientController.java
+++ src/main/java/com/pratian/PatientService/Controller/PatientController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 	import org.springframework.web.bind.annotation.RequestMethod;
 	import com.pratian.PatientService.Entities.Patient;
 	import com.pratian.PatientService.Service.IPatientService;
+
+import io.swagger.v3.oas.annotations.Operation;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,19 +23,24 @@ import com.pratian.PatientService.Entities.AppointmentHistory;
 	{
 	@Autowired
 	private IPatientService service;
-	@RequestMapping(value="/getviewallpatients/{id}",method =RequestMethod.GET)
+
+	@RequestMapping(value="/getviewallpatients/{id}",method =RequestMethod.GET)//to get a patient using particular id using get method
+	@Operation(summary = "to get patient using id")
 	public Patient showviewallpatientById(@PathVariable(value="id")long id) {
-		return service.showviewallpatientsById(id);
+		return service.showviewallpatientsById(id);// using service we got the method to call using id
 	}
 	@RequestMapping(value="/getpatients",method =RequestMethod.GET)
+	@Operation(summary = "to get all Patients")
 	public List<Patient> showviewallpatients() {
 		return service.showviewallpatients();
 	}
-	@GetMapping("/appointment/{id}")
 	
+	@GetMapping("/appointment/{id}")
+	@Operation(summary = "to get appointment using id")
 	public List<AppointmentHistory> get(@PathVariable(value="id")long id) {
-	return service.getAppointments(id);
+		return service.getAppointments(id);
 	}
+	
 }
 
 
