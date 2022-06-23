de1c628b30396c7003d1e77bbb934bcf53bdd8c1
jai.surya.r.k <jai.surya.r.k@skillassure.com>
2022-06-23 10:00:42 +0000

    Update PrescriptionController.java

--- src/main/java/com/pratian/PatientService/Controller/PrescriptionController.java
+++ src/main/java/com/pratian/PatientService/Controller/PrescriptionController.java
@@ -3,6 +3,8 @@ package com.pratian.PatientService.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pratian.PatientService.Entities.PrescriptionHistory;
+import com.pratian.PatientService.Exceptions.PrescriptionNotFoundException;
 import com.pratian.PatientService.Service.PrescriptionService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,8 +30,15 @@ public class PrescriptionController {
 		return service.getPrescription();
 		}
 		@GetMapping("/Prescription/{id}")
-		public List<PrescriptionHistory> get(@PathVariable(value="id")long id) {
-		return service.getPrescriptionById(id);
+		public ResponseEntity<?>get(@PathVariable(value = "id") long id) {
+			ResponseEntity<?> response=null;
+			try {
+				response=new ResponseEntity<>(service.getPrescriptionById(id),HttpStatus.OK);
+			}catch (PrescriptionNotFoundException e) {
+				response=new ResponseEntity<>(e.getMessage(),HttpStatus.OK);
+			}
+			
+			return response;
 
 		
 	}
