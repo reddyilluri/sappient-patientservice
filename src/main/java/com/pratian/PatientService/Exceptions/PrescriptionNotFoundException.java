444a352bfda4d617655c80774b496e84a004bb15
jai.surya.r.k <jai.surya.r.k@skillassure.com>
2022-06-23 10:59:04 +0000

    Update PrescriptionNotFoundException.java

--- src/main/java/com/pratian/PatientService/Exceptions/PrescriptionNotFoundException.java
+++ src/main/java/com/pratian/PatientService/Exceptions/PrescriptionNotFoundException.java
@@ -1,6 +1,6 @@
 package com.pratian.PatientService.Exceptions;
 
-public class PrescriptionNotFoundException {
+public class PrescriptionNotFoundException extends Exception {
 	 @SuppressWarnings("unused")
 	private static final long serialVersionUID = 1L;
 
