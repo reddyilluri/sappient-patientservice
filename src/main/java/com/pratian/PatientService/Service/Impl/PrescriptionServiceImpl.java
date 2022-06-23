82acab9354fba0ba9d65eb57005990d7ce8a7f09
jai.surya.r.k <jai.surya.r.k@skillassure.com>
2022-06-23 10:05:17 +0000

    Update PrescriptionServiceImpl.java

--- src/main/java/com/pratian/PatientService/Service/Impl/PrescriptionServiceImpl.java
+++ src/main/java/com/pratian/PatientService/Service/Impl/PrescriptionServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.pratian.PatientService.Entities.PrescriptionHistory;
 import com.pratian.PatientService.Exceptions.PrescriptionNotFoundException;
+import com.pratian.PatientService.Repository.IPatientRepo;
 import com.pratian.PatientService.Repository.PrescriptionRepo;
 import com.pratian.PatientService.Service.PrescriptionService;
 
@@ -14,6 +15,8 @@ public class PrescriptionServiceImpl implements PrescriptionService{
 @Autowired
 private PrescriptionRepo repo;
 
+@Autowired
+private IPatientRepo prepo;
 
 @Override
 public List<PrescriptionHistory> getPrescription() {	
@@ -21,9 +24,12 @@ public List<PrescriptionHistory> getPrescription() {
 return repo.getPrescription();
 }
 @Override
-public PrescriptionHistory getPrescriptionById(long id) {
-
-return repo.getPrescriptionById(id);
+public List<PrescriptionHistory> getPrescriptionById(long id) throws PrescriptionNotFoundException {
+	List<PrescriptionHistory> prescriptions =  prepo.showviewallpatientById(id).getPrescriptionhistory();
+	if(prescriptions.isEmpty()) {
+		throw new PrescriptionNotFoundException("Prescription history is not found");
+	}
+return prescriptions;
 }
 
 
