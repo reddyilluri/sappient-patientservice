7db5b8d2c603a5a65b425233f8d89c99d5aac8fe
jai.surya.r.k <jai.surya.r.k@skillassure.com>
2022-06-23 10:24:32 +0000

    Update AppointmentHistoryService.java

--- src/main/java/com/pratian/PatientService/Service/AppointmentHistoryService.java
+++ src/main/java/com/pratian/PatientService/Service/AppointmentHistoryService.java
@@ -3,17 +3,13 @@ package com.pratian.PatientService.Service;
 import java.util.List;
 
 import com.pratian.PatientService.Entities.AppointmentHistory;
-import com.pratian.PatientService.Entities.Patient;
-import com.pratian.PatientService.Entities.PrescriptionHistory;
+import com.pratian.PatientService.Exceptions.AppointmentHistoryNotFoundException;
 
 
 public interface AppointmentHistoryService  {
 	
 	public List<AppointmentHistory> showallAppointmentHistory();
 	
-	public AppointmentHistory getAppointmentHistoryById(long id);
+	public List<AppointmentHistory> getAppointmentHistoryById(long id) throws AppointmentHistoryNotFoundException;
 	
 }
-
-
-
