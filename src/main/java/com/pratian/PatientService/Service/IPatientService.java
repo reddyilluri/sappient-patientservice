73acec98a24f4e9792806bc94933dea3ab5c5be8
Manduru.Venkateswarlu <manduru.venkateswarlu@skillassure.com>
2022-06-30 09:45:48 +0000

    Update IPatientService.java

--- src/main/java/com/pratian/PatientService/Service/IPatientService.java
+++ src/main/java/com/pratian/PatientService/Service/IPatientService.java
@@ -4,28 +4,29 @@ import java.util.List;
 
 import com.pratian.PatientService.Entities.AppointmentHistory;
 import com.pratian.PatientService.Entities.Patient;
+import com.pratian.PatientService.Exceptions.PatientAlreadyExistsException;
 import com.pratian.PatientService.Exceptions.PatientNotFoundException;
 
 public interface IPatientService {
 	public List<Patient> showviewallpatients();
-		
+
 //	public Patient showviewallpatientsById(long id);
-	
+
 //	public List<AppointmentHistory> getAppointments(long id);
-	
+
 //	public List<AppointmentHistory> showallAppointmentHistory();
-	
+
 //	public list<appointmentHistory> showallAppointmentdetails();
-	
-	//public List<AppointmentHistory> getAppointments(long id);
 
-		// we have to get the patients details by Id
-		Patient getPatientDetails(long id) throws PatientNotFoundException;
+	// public List<AppointmentHistory> getAppointments(long id);
 
-		// we get save the patient details
-		Patient savePatientDetails(Patient patient) throws PatientNotFoundException, PatientNotFoundException;
+	// we have to get the patients details by Id
+	Patient getPatientDetails(long id) throws PatientNotFoundException;
 
-		// we can edit the patient details
-		Patient editPatientDetails(Long patientId, Patient patient) throws PatientNotFoundException;
-	
+	// we get save the patient details
+	Patient savePatientDetails(Patient patient) throws PatientAlreadyExistsException;
+
+	// we can edit the patient details
+	Patient editPatientDetails(Long patientId, Patient patient) throws PatientNotFoundException;
+
 }
